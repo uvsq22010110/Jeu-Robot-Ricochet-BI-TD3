2552e74bce11a4465d053688fa7ab02cd73de66b
Coraline PELON <coraline.pelon@ens.uvsq.fr>
2021-03-02 10:44:27 +0100

    coucou

--- incendie.py
+++ incendie.py
@@ -1,30 +1,13 @@
 ##################################
 # Groupe 1  de BI TD3
 
-# Jacques-Henri Lartigue           
-
-# Nojimba Ahamada
-
-#
-
-#
-#
-
 # Auteurs: 
 # Coraline PELON
 # Jacques-Henri Lartigue 
 # Nojimba Ahamada
-
+#
 # Mathieu Chekroun
 #
-#
 
-# Groupe: 
-# BI TD3
-
-print ("hello")
-print ("123")
-print("Test")
 
 # lien du github: https://github.com/uvsq22010110/Projet_Incendie.git
-
