0235af3a2f46f166221384ab782edb7d175e6977
N.AHAMADA <71550445+uvsq22000221@users.noreply.github.com>
2021-03-04 17:21:48 +0100

    Update incendie.py

--- incendie.py
+++ incendie.py
@@ -14,6 +14,12 @@
 # import des modules
 
 import tkinter as tk
+import random
+#Initialisation
+
+tableau = None
+
+cases = []
 
 # constantes
 
@@ -23,28 +29,68 @@ COULEUR_FOND = "grey60"
 COTE = 30
 COULEUR_QUADR = "grey20"
 
+NB_COL = LARGEUR // COTE
+NB_LINE = HAUTEUR // COTE
+
 COULEUR_FORET = "green"
 COULEUR_EAU = "blue"
 COULEUR_PRAIRIE = "yellow"
 COULEUR_CENDRES_TIEDES = "grey80"
 COULEUR_CENDRES_ETEINTES = "black"
 COULEUR_FEU = "red"
-
+COULEUR_ELEM = ["yellow", "red", "blue", "black", "grey80", "green"]
 # fonctions
 
 def quadrillage():
-    """Dessine un quadrillage dans le canvas avec des carrés de côté COTE"""
+    """Affiche un quadrillage sur le canvas."""
+    x0, x1 = 0, LARGEUR
     y = 0
     while y <= HAUTEUR:
-        canvas.create_line((0, y), (LARGEUR, y), fill=COULEUR_QUADR)
+        canvas.create_line(x0, y, x1, y, fill=COULEUR_QUADR)
         y += COTE
-    i = 0
-    while i*COTE <= LARGEUR:
-        x = i*COTE
-        canvas.create_line((x, 0), (x, HAUTEUR), fill=COULEUR_QUADR)
-        i += 1
+    y0, y1 = 0, LARGEUR
+    x = 0
+    while x <= LARGEUR:
+        canvas.create_line(x, y0, x, y1, fill=COULEUR_QUADR)
+        x += COTE
+        
+def coord_to_lg(x, y):
+    """Fonction qui retourne la colonne et la ligne du quadrillage
+    à partir des coordonnées x et y"""
+    return x // COTE, y // COTE        
 
+def feu(event):
+   """L'utilisateur entame un incendie par un click""" 
+    i, j = coord_to_lg(event.x, event.y)
+    if tableau[i][j] == -1:
+        x, y = i * COTE, j * COTE
+        carre = canvas.create_rectangle(x, y, x + COTE,
+                                        y + COTE, fill=COULEUR_FEU,
+                                        outline=COULEUR_QUADR)
+        tableau[i][j] = carre
+    else:
+        canvas.itemconfig(tableau[i][j], fill = COULEUR_CENDRES_ETEINTES )
+        tableau[i][j] = -1
 
+def creer_tableau():
+    """initialise un tableau à deux dimensions qui vaut -1 partout
+    -1 est pour une case morte
+    identifiant du carré dessiné si une case est vivante
+    tableau[i][j] est la valeur de la case à la colonne i et la ligne j
+    """
+    global tableau
+    tableau = []
+    for i in range(NB_COL):
+        tableau.append([-1] * NB_LINE)
+
+def load_map():
+  """Chargement d'une map aléatoire"""  
+    for i in range(COTE):
+        for j in range(COTE):
+            x, y = i * COTE, j * COTE
+            carre = canvas.create_rectangle(x, y, x + COTE, y + COTE, fill = random.choice(COULEUR_ELEM), outline=COULEUR_QUADR)
+
+        
 # programme principal
 
 racine = tk.Tk()
@@ -57,22 +103,20 @@ canvas = tk.Canvas(racine, width=LARGEUR, height=HAUTEUR, bg=COULEUR_FOND)
 
 bouton_play = tk.Button(text = "PLAY", font = ("Times", "15"), bg = "white")
 bouton_save = tk.Button(text = "SAVE", font = ("Times", "15"), bg = "white")
-bouton_load = tk.Button(text = "LOAD", font = ("Times", "15"), bg = "white")
+bouton_load = tk.Button(text = "LOAD", font = ("Times", "15"), bg = "white", command = load_map)
 bouton_end = tk.Button(text = "END", font = ("Times", "15"), bg = "white")
 bouton_quit = tk.Button(text = "QUIT", font = ("Times", "15"), bg = "white",command=racine.quit)
-<<<<<<< HEAD
+
 #
 #bouton_fire = tk.Button(text = "FIRE", font = ("Times", "15"), bg = "red")
 #bouton_water = tk.Button(text = "WATER", font = ("Times", "15"), bg = "blue")
 #bouton_forest = tk.Button(text = "FOREST", font = ("Times", "15"), bg = "green")
 #
 
-=======
 
 bouton_fire = tk.Button(text = "FIRE", font = ("Times", "15"), bg = "red")
 bouton_water = tk.Button(text = "WATER", font = ("Times", "15"), bg = "blue")
 bouton_forest = tk.Button(text = "FOREST", font = ("Times", "15"), bg = "green")
->>>>>>> 7a4f48344cc8bf3b8600996fb725227590ce4af9
 
 
 # placement des widgets
@@ -88,7 +132,12 @@ bouton_quit.grid(column = 4, row = 1)
 #bouton_water.grid(column = 1, row = 2)
 #bouton_forest.grid(column = 2, row = 2)
 
+#Assignation des commandes aux touches du clavier ou souris
+canvas.bind("<Button-1>", feu)
+
+
+
 # liaison des évènements
 quadrillage()
-
+creer_tableau()
 racine.mainloop()
