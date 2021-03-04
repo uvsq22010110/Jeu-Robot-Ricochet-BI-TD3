ab446261a7761e0fe6435ca69dd13a2becfc8ec1
Coraline PELON <coraline.pelon@ens.uvsq.fr>
2021-03-04 10:27:09 +0100

    coucou

--- incendie.py
+++ incendie.py
@@ -6,15 +6,6 @@
 # Jacques-Henri Lartigue 
 # Nojimba Ahamada
 # ALice Dumontroty
-<<<<<<< HEAD
-
 # Mathieu Chekroun
-# Alice Dumontroty 
-=======
-#
-# Mathieu Chekroun
-#
->>>>>>> bd7fd2e7ffd06df65e0d0d18316f77122e158b4e
-
-
+# Desira
 # lien du github: https://github.com/uvsq22010110/Projet_Incendie.git
