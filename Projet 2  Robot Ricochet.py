0d3c7682049f2598e057b7868c95b0b2a5f7d079
uvsq22010110 <71687783+uvsq22010110@users.noreply.github.com>
2021-04-21 21:46:41 +0200

    Add files via upload

---
+++ Projet 2  Robot Ricochet.py
@@ -0,0 +1,33 @@
+##################################
+# Groupe 1  de BI TD3
+
+# Auteurs: 
+# Coraline PELON
+# Jacques-Henri Lartigue 
+# Nojimba Ahamada
+# Alice Dumontroty
+# Mathieu Chekroun
+# Desira Junior Ebelebe
+
+# lien du github:https://github.com/uvsq22010110/Projet-2-Robot-Ricochet.git
+
+# Comment le programme fonctionne :
+
+# import des modules
+
+import tkinter as tk
+
+# constantes
+
+
+# programme principal
+
+racine = tk.Tk()
+racine.title("Jeu des robots")
+
+# création des widgets
+canvas = tk.Canvas(racine, width= 640, height= 640, bg= "black")
+canvas.grid()
+racine.mainloop()
+
+print("hello :)")
