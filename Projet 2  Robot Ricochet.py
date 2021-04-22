c0be894e61b1c5ea5edfc3c49b3bbf8e9ec41b75
Coraline PELON <coraline.pelon@ens.uvsq.fr>
2021-04-22 07:39:17 +0200

    liaison des flèches liées aux robots

--- Projet 2  Robot Ricochet.py
+++ Projet 2  Robot Ricochet.py
@@ -79,6 +79,31 @@ def creer_caree():
     caree = canvas.create_rectangle((40*(i - 1), 40*(i - 1)), (40*j, 40*j), fill = "black")
 
 
+def est_dans_le_robot(event):
+    """Retourne True si on a cliqué sur un robot sinon False"""
+    print("Tu as cliqué sur un robot")
+
+
+def deplacement_gauche(event):
+    """Déplace notre robot vers la gauche lorsque l'on appui sur la touche "flèche gauche" """
+    print("Tu as appuyé sur la flèche gauche")
+
+
+def deplacement_droite(event):
+    """Déplace notre robot vers la droite lorsque l'on appui sur la touche "flèche droite" """
+    print("Tu as appuyé sur la flèche droite")
+
+
+def deplacement_haut(event):
+    """Déplace notre robot vers le haut lorsque l'on appui sur la touche "flèche du haut" """
+    print("Tu as appuyé sur la flèche du haut")
+
+
+def deplacement_bas(event):
+    """Déplace notre robot vers le bas lorsque l'on appui sur la touche "flèche du bas" """
+    print("Tu as appuyé sur la flèche du bas")
+
+
 # programme principal
 racine = tk.Tk()
 racine.title("Jeu des robots")
@@ -86,8 +111,17 @@ racine.title("Jeu des robots")
 # création des widgets
 canvas = tk.Canvas(racine, width= LARGEUR, height= HAUTEUR, bg= "navajo white")
 
+# liaison des événements
+canvas.bind("<Button-1>", est_dans_le_robot)
+racine.bind("<KeyPress-Left>", deplacement_gauche)
+racine.bind("<KeyPress-Right>", deplacement_droite)
+racine.bind("<KeyPress-Down>", deplacement_bas)
+racine.bind("<KeyPress-Up>", deplacement_haut)
+
+# placement des widgets
 canvas.grid()
 
+# programme principal
 creer_robot(COULEUR_ROBOT1)
 creer_robot(COULEUR_ROBOT2)
 creer_robot(COULEUR_ROBOT3)
@@ -95,4 +129,5 @@ creer_robot(COULEUR_ROBOT4)
 
 creer_caree()
 quadrillage()
+
 racine.mainloop()
